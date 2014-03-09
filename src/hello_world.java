8d14df1647e400883f2c4cf308007b852bfad5d7
Thang Nguyen <graham.tatthang@gmail.com>
2014-03-09 01:27:01 -0800

    fourth commit

--- src/hello_world.java
+++ src/hello_world.java
@@ -6,6 +6,7 @@ public class hello_world {
 		System.out.println("Hello world");
 		System.out.println("Second commit");
 		System.out.println("Third commit");
+		System.out.println("Fourth commit");
 	}
 
 }
